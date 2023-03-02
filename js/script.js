6d86c39f5b4160a542c292815e8652c4b6d65f73
seaneidref <seaneidref.natividad@sdca.edu.ph>
2023-03-02 09:09:33 +0800

    back to top added and jquery cdn

--- js/script.js
+++ js/script.js
@@ -1,3 +1,16 @@
 $(".hexagon").click(function () {
   window.location.href = this.getElementsByClassName("mylink")[0].href;
 });
+
+onscroll = (event) => {
+  console.log("scrolling")
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
+    $("#back").show()
+  } else {
+    $("#back").hide()
+  }
+}
